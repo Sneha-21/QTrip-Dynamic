97c95c37acd1d8fc70eab78a41d60013ef98361a
CrioUser <crio-user@criodo.com>
2023-03-10 17:17:05 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURE_DETAILS

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://13.126.244.91:8082" };
+const config = { backendEndpoint: "http://3.109.51.127:8082" };
 
 export default config;
--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -4,33 +4,105 @@ import config from "../conf/index.js";
 function getAdventureIdFromURL(search) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Get the Adventure Id from the URL
-
-
+  let params = new URLSearchParams(search);
+  //console.log(params.get('adventure'));
+  
   // Place holder for functionality to work in the Stubs
-  return null;
+  return params.get('adventure');
 }
 //Implementation of fetch call with a paramterized input based on adventure ID
 async function fetchAdventureDetails(adventureId) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Fetch the details of the adventure by making an API call
-
-
+  try {
+    let adventureDetail = await fetch(`${config.backendEndpoint}/adventures/detail?adventure=${adventureId}`);
+    //console.log(adventureDetail.json());
+    let data = await adventureDetail.json();
+    //console.log(data);
+    return data;
+  } catch (error) {
+      return null;
+  }
   // Place holder for functionality to work in the Stubs
-  return null;
+  //return adventureDetails.json();
 }
 
 //Implementation of DOM manipulation to add adventure details to DOM
 function addAdventureDetailsToDOM(adventure) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the details of the adventure to the HTML DOM
+  let adventureName = document.getElementById("adventure-name");
+  adventureName.innerHTML = adventure.name;
 
+  let adventureSubtitle = document.getElementById("adventure-subtitle");
+  adventureSubtitle.innerHTML = adventure.subtitle;
+
+  let adventureImages = document.getElementById("photo-gallery");
+  adventure.images.forEach(image => {
+    let adventureImg = document.createElement("div");
+    
+    adventureImg.innerHTML = `
+      <img src = ${image} alt = ${adventure.name} class = "activity-card-image">
+    `;
+
+    adventureImages.append(adventureImg);
+    });
+
+    let adventureContent = document.getElementById("adventure-content");
+    adventureContent.innerHTML = adventure.content;
+  
 }
 
 //Implementation of bootstrap gallery component
 function addBootstrapPhotoGallery(images) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
+  let adventureImages = document.getElementById("photo-gallery");
+  adventureImages.innerHTML = `
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators"></div>
+      <div class="carousel-inner"></div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  `;
 
+  let carouselButtonElement = document.querySelector(".carousel-indicators");
+  let carouselInnerElement = document.querySelector(".carousel-inner");
+  let count = 1;
+  let adventureImg;
+  images.forEach(image => {
+    carouselButtonElement.innerHTML += ` 
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to= "${count - 1}"  aria-label="Slide ${count}" id="button${count}"></button>
+    `;
+    /* if(count == 1) {
+      document.getElementsByTagName("button")[0].setAttribute("class","active");
+      document.getElementsByTagName("button")[0].setAttribute("aria-current","true");
+    } */
+    adventureImg = document.createElement("div");
+    adventureImg.className = "carousel-item";
+    adventureImg.setAttribute("id",`adImage${count}`);
+    adventureImg.innerHTML = `
+      <img src = ${image}  class="d-block w-100 activity-card-image">
+    `;
+    count++;
+    //console.log(image);
+    /* let activeImage = document.getElementById(adImage1);
+    activeImage.setAttribute("class","active"); */
+    carouselInnerElement.append(adventureImg);
+    });
+
+    let activeImage = document.getElementById("adImage1");
+    activeImage.className = "carousel-item active";
+    document.getElementById("button1").setAttribute("class","active");
+    document.getElementById("button1").setAttribute("aria-current","true");
+    //console.log(activeImage);
 }
 
 //Implementation of conditional rendering of DOM based on availability
